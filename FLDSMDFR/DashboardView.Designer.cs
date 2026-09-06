72a26186865f319c6cc47f4b55e7802eabdef541
AdamLeymeister <adam.leymeister@gmail.com>
2026-09-06 17:34:02 -0500

    table

--- FLDSMDFR/DashboardView.Designer.cs
+++ FLDSMDFR/DashboardView.Designer.cs
@@ -28,22 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.tlpDashboard = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlContent.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.Controls.Add(this.tlpDashboard);
+            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent.Location = new System.Drawing.Point(0, 0);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Padding = new System.Windows.Forms.Padding(20);
+            this.pnlContent.Size = new System.Drawing.Size(1359, 783);
+            this.pnlContent.TabIndex = 0;
+            // 
+            // tlpDashboard
+            // 
+            this.tlpDashboard.ColumnCount = 3;
+            this.tlpDashboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpDashboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpDashboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpDashboard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpDashboard.Location = new System.Drawing.Point(20, 20);
+            this.tlpDashboard.Name = "tlpDashboard";
+            this.tlpDashboard.RowCount = 2;
+            this.tlpDashboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpDashboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpDashboard.Size = new System.Drawing.Size(1319, 743);
+            this.tlpDashboard.TabIndex = 0;
             // 
             // DashboardView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 45F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(16)))), ((int)(((byte)(22)))));
+            this.Controls.Add(this.pnlContent);
             this.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.Color.Snow;
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "DashboardView";
-            this.Size = new System.Drawing.Size(270, 270);
+            this.Size = new System.Drawing.Size(1359, 783);
+            this.pnlContent.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Panel pnlContent;
+        private TableLayoutPanel tlpDashboard;
     }
 }
